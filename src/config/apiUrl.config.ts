ecbb5f194d5bb5a6ae319dbf6ea2045741203c47
machmaegal <aka.pleat371@passinbox.com>
2025-01-15 14:51:02 +0100

    add config for deploy

---
+++ src/config/apiUrl.config.ts
@@ -0,0 +1,6 @@
+const APIURL = import.meta.env.VITE_APIURL || "http://localhost:5005";
+export { APIURL };
+
+//import at app.jsx
+//create redirects in public
+//do npm run build (look in package) -> builds dist folder
